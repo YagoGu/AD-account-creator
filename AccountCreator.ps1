762cf9d292a9fa413b9828b947b49e6bb2f6931a
YagoGu <yago.gutierrez15@gmail.com>
2024-05-14 23:41:30 -0700

    testing

---
+++ AccountCreator.ps1
@@ -0,0 +1,57 @@
+function New-OneOffADUser{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory)]
+        [string]$FirstName,
+        [Parameter(Mandatory)]
+        [string]$LastName,
+        [Parameter(Mandatory)]
+        [string]$UserName,
+        [Parameter(Mandatory)]
+        [string]$Reason,
+        [Parameter(Mandatory)]
+        [string]$Server,
+        [Parameter()]
+        [datetime]$ExpirationDate,
+        [Parameter()]
+        [int]$PasswordLength=15
+    )
+
+    <#Checks if expiration date is before current date#>
+    try {
+        if($ExpirationDate){
+            $Date=Get-Date -Date $ExpirationDate
+        }
+
+        $PlainTextPassword= -Join (@('0'..'9';'A'..'Z';'a'..'z';'@';'!';'#';'&') | Get-Random -Count $PasswordLength)
+        $Password=ConvertTo-SecureString -String $PlainTextPassword -AsPlainText -Force
+    
+        $ADUserParams=@{
+            Name=$UserName
+            GivenName=$FirstName
+            SurName=$LastName
+            SamAccountName=$UserName
+            UserPrincipalName="$Username@wservertest.local"
+            Description=$Reason
+            Title=$Reason
+            Enabled=$true
+            AccountPassword=$Password
+            Server=$Server
+        }
+
+        if($Date){
+            New-ADUser @ADUserParams -ExpirationDate $Date
+        }
+        else{
+            New-ADUser @ADUserParams
+        }
+
+        Write-Output "User create for $FirstName $LastName with the username: $Username and password: $PlainTextPassword"
+    }
+    catch {
+        Write-Error $_.Exception.Message
+    }
+}
+
+<#Create the user#>
+New-OneOffADUser -FirstName "Yago" -LastName "Gutierrez" -UserName "yagouser" -Reason "Test" -Server "wservertest.local" -ExpirationDate "2024-5-31"
